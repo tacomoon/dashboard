d212c6d9478de1cc0dfe578c3c8e0488298d4ba9
eugene.gorbachev <evlgorbachev@gmail.com>
2021-11-04 23:43:35 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+    static: {
+      directory: path.resolve('dist'),
+    },
+  },
+}
